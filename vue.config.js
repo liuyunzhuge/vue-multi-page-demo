9fc27d7fa38952aa59ca9d050bc3ecd41f799a21
liuyunzhuge <imzengxf@163.com>
2019-12-19 23:20:41 +0800

    create demo

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const createPage = (name, title, chunk = '')=>{
+    return {
+      entry: `src/pages/${name}/main.js`,
+      template: `public/${name}.html`,
+      filename: `${name}.html`,
+      title,
+      chunks: ['chunk-vendors', 'chunk-common', chunk || name]
+    }
+}
+
+module.exports = {
+   pages: {
+    index: createPage('index', '首页'),
+    about: createPage('about', '关于我们'),
+    'some_module': createPage('some/module', '某一个子页面', 'some_module'),
+  }
+}
